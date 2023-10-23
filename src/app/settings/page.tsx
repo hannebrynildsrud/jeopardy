ead83b720c930f213c1d5739c3349256005efe4a
Christopher Hærem <christopher.haerem@computas.com>
2023-10-23 15:59:34 +0200

    La til sikkerhet

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { v4 as uuidv4 } from "uuid";
-import { useState } from "react";
+import { useState, useEffect } from "react"; // Import useEffect
 import styles from "./page.module.scss";
 import { Admin } from "./admin/admin";
 import { Category, Game, GameState } from "../models/interfaces";
@@ -12,8 +12,24 @@ export default function Settings() {
   const options: number[] = [1, 2, 3, 4, 5];
   const [numOfCategories, setNumOfCategories] = useState<number>(1);
   const [categories, setCategories] = useState<Category[]>([]);
+  const [authenticated, setAuthenticated] = useState(
+    localStorage.getItem("authenticated") === "true" // Check local storage for authentication state
+  );
 
   const { game, updateGameState, resetGame } = useGameContext();
+
+  useEffect(() => {
+    if (!authenticated) {
+      // Only show prompt if not already authenticated
+      const password = prompt("Please enter your password:", "");
+      if (password === "sjøpølse") {
+        setAuthenticated(true);
+        localStorage.setItem("authenticated", "true"); // Set local storage on correct password
+      } else {
+        alert("Incorrect password.");
+      }
+    }
+  }, [authenticated]); // Depend on authenticated state
 
   const handleNumberOfCategories = (event: any) => {
     setNumOfCategories(event.target.value);
@@ -72,36 +88,40 @@ export default function Settings() {
 
   return (
     <main>
-      <h1>Innstillinger</h1>
-      {game?.gameState === GameState.GAME_SETUP ? (
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <label className={styles.subtitle}>
-            Hvor mange kategorier?
-            <select
-              onChange={handleNumberOfCategories}
-              id="select"
-              value={numOfCategories}
-              className={styles.form_field}
-            >
-              {options.map((e) => (
-                <option key={e} value={e}>
-                  {e}
-                </option>
-              ))}
-            </select>
-          </label>
-          {numOfCategories > 0 && renderCategories()}
-          <button
-            className={styles.game_button}
-            type="submit"
-            name="save_categories"
-          >
-            Åpne registrering
-          </button>
-        </form>
-      ) : (
-        <Admin resetGame={resetGame} />
-      )}
+      {authenticated ? ( // Render content only if authenticated
+        <>
+          <h1>Innstillinger</h1>
+          {game?.gameState === GameState.GAME_SETUP ? (
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <label className={styles.subtitle}>
+                Hvor mange kategorier?
+                <select
+                  onChange={handleNumberOfCategories}
+                  id="select"
+                  value={numOfCategories}
+                  className={styles.form_field}
+                >
+                  {options.map((e) => (
+                    <option key={e} value={e}>
+                      {e}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              {numOfCategories > 0 && renderCategories()}
+              <button
+                className={styles.game_button}
+                type="submit"
+                name="save_categories"
+              >
+                Åpne registrering
+              </button>
+            </form>
+          ) : (
+            <Admin resetGame={resetGame} />
+          )}
+        </>
+      ) : null}
     </main>
   );
 }
